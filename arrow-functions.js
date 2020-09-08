70196be28dbc8d58cfca33f8536fdfae40513db0
Andressa28 <andressacportela@gmail.com>
2020-09-08 18:04:33 +0000

    Aula 03 - Arrow-functions.js

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,10 +1,9 @@
 // Expressão de função "tradicional"
 
 // Características desta função
-// - Um único argumento
+// - Um único argumento -> n
 // - Seu corpo tem apenas uma linha de código com return
-
-let quadrado = function(n){
+let quadrado = function(n) {
     return n * n
 }
 
@@ -13,7 +12,6 @@ let quadrado = function(n){
 // - O argumento não precisa de parênteses
 // - Após o argumento há a flecha (arrow) =>
 // - As chaves e a palavra return são omitidos
-
 let quadrado2 = n => n * n
 
 console.log(quadrado(6))
@@ -22,7 +20,6 @@ console.log(quadrado2(6))
 // Situação alternativa 1: função sem argumento
 
 // Retorna um número aleatório entre 0 e 999
-
 let aleatorio = function() {
 // Math.random() -> gera um número aleatório entre 0 e 1 (fracionário)
 // Math.floor() -> "corta fora" as casas decimais de um número
@@ -33,15 +30,19 @@ console.log('----------------------------------------')
 console.log(aleatorio())
 
 // Arrow function com 0 argumento
-// - Os parênteses vazios marcam o lugar do argumento (obrigatório)
-
+// - Os parênteses vazios marcam o lugar do argumento (obrigatórios)
 let aleatorio2 = () => Math.floor(Math.random() * 1000)
 
 // Situação alternativa 2: função com 1+ argumento
 
 // Função tradicional com 1+ argumento
+let imc = function(peso, altura) {
+    return peso / (altura ** 2)
+}
+
+// Arrow function com 1+ argumento
 // Os parênteses dos argumentos são obrigatórios quando o número de arguemtnos > 1
-let imc2 = (peso, altura) => peso / altura ** 2)
+let imc2 = (peso, altura) => peso / (altura ** 2)
 
 console.log(imc(87, 1.78))
 console.log(imc2(87, 1.78))
@@ -56,6 +57,7 @@ console.log(imc2(87, 1.78))
 let fatorial = function(n) {
     let res = 1
     for(let i = n; i > 1 ; i--) {
+        res *= i
     }
     return res
 }
@@ -69,5 +71,8 @@ let fatorial2 = n => {
     }
     return res
 }
+
 console.log(fatorial(5))
+// console.log(fatorial2(5))
+// ou
 console.log(`O resultado é ${fatorial2(5)}.`)
