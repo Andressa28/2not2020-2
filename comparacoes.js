b0c9a51011136b5c564a77110844b866cf047c46
Andressa28 <andressacportela@gmail.com>
2020-09-08 17:44:26 +0000

    Aula 02 - Comparações.js

--- comparacoes.js
+++ comparacoes.js
@@ -1,9 +1,11 @@
+// Comparando dois números distintos
 let a = 10, b = 7
 console.log(a > b)
 console.log(b > a)
 
-// Comparando duas strings distintas
+console.log('----------------------------------')
 
+// Comparando duas strings distintas
 let x = 'abacaxi', y = 'jabuticaba'
 console.log(x > y)
 console.log(y > x)
@@ -11,11 +13,19 @@ console.log(y > x)
 console.log('----------------------------------')
 
 // Comparando dois objetos
+let carro1 = { modelo: 'Fusca', marca: 'Volkswagen', ano: 1969 }
+let carro2 = { modelo: 'Palio', marca: 'Fiat', ano: 2011 }
+console.log(carro1 > carro2)
+console.log(carro2 > carro1)
+console.log(carro1 == carro2)
 
+console.log('----------------------------------')
+
+// Comparando booleanos
 console.log(true > false)
 console.log(false > true)
 
-// Tipos de dados comparáveis em JS
-// Number
-// String
-// Boolean
+// MORAL DA HISTÓRIA: Tipos de dados comparáveis em JS são apenas
+// - Number
+// - String
+// - Boolean
