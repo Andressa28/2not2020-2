7bcea0b97ea58fa89941ae922551c2b5b0c7f8d4
Andressa28 <andressacportela@gmail.com>
2020-09-14 16:30:22 +0000

    Aula 03 - Ativ01 - Revisão JavaScript

--- ativ01-revisao-javascript.js
+++ ativ01-revisao-javascript.js
@@ -40,7 +40,6 @@ let estado2 = {
 
 estadosNe.push(estado2)
 
-
 let estado3 = {
    nome: 'Rio Grande do Norte',
    sigla: 'RN',
@@ -58,6 +57,7 @@ let estado4 = {
 }
 
 estadosNe.push(estado4)
+
 
 let estado5 = {
    nome: 'Pernambuco',
@@ -104,14 +104,18 @@ let estado9 = {
 
 estadosNe.push(estado9)
 
+// Exibindo o vetor para verificar se está tudo certo
+console.log(estadosNe)
+
 /*
    2) Escreva uma arrow function que receba dois parâmetros, área e população, e
       calcule a densidade demográfica retornando o resultado da divisão da
       segunda pela primeira. 
 */
-const densidadeDemog = (area, pop) => pop / area
 
-console.log(densidadeDemog)
+// Usei const aqui porque densidadeDemog deve receber um valor inicial
+// e esse valor não será mais alterado
+const densidadeDemog = (area, pop) => pop / area
 
 /*
    3) Percorra o vetor estadosNe usando um for tradicional. Para cada estado,
@@ -122,6 +126,7 @@ console.log(densidadeDemog)
       Durante este mesmo loop, elimine a propriedade 'sigla' dos objetos.
 
 */
+
 for(let i = 0; i < estadosNe.length; i++) {
    // Acrescentando a nova propriedade "densidade demográfica".
    // O nome contém duas palavras, então a sintaxe de colchetes é obrigatória.
@@ -136,13 +141,28 @@ for(let i = 0; i < estadosNe.length; i++) {
 
 */
 
+const listaEstado = estado => {
+   for(let prop in estado) {
+      console.log(`${prop} => ${estado[prop]}`)
+   }
+}
 
 /* 5) Percorra o vetor estadosNe usando for..of. Para cada objeto no vetor,
       invoque a função escrita em 4) para exibi-lo.
+*/
+
+// Traço separador
+console.log('-----------------------------------------------')
+
+for(let est of estadosNe) {
+   listaEstado(est)
+   // Traço separador
+   console.log('-----------------------------------------------')
+}
 
 /*
-
-   6) a) Declare um vetor vazio.
+   6)
+      a) Declare um vetor vazio.
       b) Insira no vetor criado no item a) apenas o nome de cada Estado, conforme
          os dados no topo deste arquivo, um de cada vez. Faça as inserções de 
          modo que o vetor final esteja em ordem alfabética. No caso, o primeiro 
@@ -150,5 +170,38 @@ for(let i = 0; i < estadosNe.length; i++) {
          deve ser inserido com unshift(), para manter a ordem alfabética. 
          O terceiro Estado (Rio Grande do Norte) deve ser inserido entre os dois 
          já existentes, e assim por diante.
-
 */
+
+let nomesUf = []
+
+// Sergipe => pode ser adicionado com push(), visto que o vetor está vazio
+nomesUf.push('Sergipe')
+
+// Alagoas => deve ser inserido com unshift() para vir antes de Alagoas
+nomesUf.unshift('Alagoas')
+
+// Rio Grande do Norte => com splice(), entre os dois já existentes
+nomesUf.splice(1, 0, 'Rio Grande do Norte')
+
+// Paraíba => com splice(), antes de RN
+nomesUf.splice(2, 0, 'Paraíba')
+
+// Pernambuco => com splice(), antes de SE
+nomesUf.splice(3, 0, 'Pernambuco')
+
+// Ceará => com splice(), antes de RN
+nomesUf.splice(1, 0, 'Ceará')
+
+// Piauí => com splice(), antes de SE
+nomesUf.splice(5, 0, 'Piauí')
+
+// Maranhão => com splice(), antes de RN
+nomesUf.splice(2, 0, 'Maranhão')
+
+// Bahia => com splice(), antes de CE
+nomesUf.splice(1, 0, 'Bahia')
+
+// Traço separador
+console.log('***********************************************')
+
+console.log(nomesUf)
